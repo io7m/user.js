5eeb0f1b55d14e7720142e05f4e9ad32160da040
Mark Raynsford <code@io7m.com>
2018-05-23 13:21:37 +0100

    Re-enable SVG

--- user.js
+++ user.js
@@ -306,7 +306,7 @@ user_pref("gfx.font_rendering.opentype_svg.enabled",		false);
 // NOTICE: Disabling SVG support breaks many UI elements on many sites
 // https://bugzilla.mozilla.org/show_bug.cgi?id=1216893
 // https://github.com/iSECPartners/publications/raw/master/reports/Tor%20Browser%20Bundle/Tor%20Browser%20Bundle%20-%20iSEC%20Deliverable%201.3.pdf#16
-user_pref("svg.disabled", true);
+user_pref("svg.disabled", false);
 
 
 // PREF: Disable video stats to reduce fingerprinting threat
